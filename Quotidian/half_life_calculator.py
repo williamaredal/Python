c754c749b1ecea3e938fa2ebb77e9d1554b57ea0
William Peter Åredal <williamaredal@hotmail.com>
2021-05-19 18:41:15 +0200

    Added function

--- Quotidian/half_life_calculator.py
+++ Quotidian/half_life_calculator.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
 
 # NB! I do not have medical experience with drug half-life so take this calculator with a grain of salt
 
@@ -22,4 +23,30 @@ def HowManyHalfCycles(dose, halfLifeTimeHours, requirement):
     return('With a half-life of ' + str(halfLifeTimeHours) + ' hours, it took ' + str(cycles) + ' cycles before reaching <5% of the original dose of ' + str(dose) + '. Meaning you would no longer feel affected at ' + str(todayReference))
 
 
-print(HowManyHalfCycles(200, 4, 0.05))
+# function that finds out how much of your drug is in your system each day when doses are taken with a given interval (1 day to 31 day intervals)
+doseOverTime = []
+def RecursiveDoseInterval(dose, doseHourInterval, dosesN, halfLifeHours, bodyDosage=0, dosesTaken=0):
+    if (dosesTaken < dosesN):
+        previousDoseInterval = datetime.now() + timedelta(hours=(doseHourInterval * (0 if ((dosesTaken - 1) < 0) else (dosesTaken - 1)) ))
+        currentDoseInterval = datetime.now() + timedelta(hours=(doseHourInterval * dosesTaken))
+
+        halfLifeTimes = ((currentDoseInterval - previousDoseInterval).total_seconds() / 3600 / halfLifeHours)
+        currentBodyDosage = (bodyDosage / (2**(halfLifeTimes) ) + dose)
+
+        doseOverTime.append((currentDoseInterval.strftime('%d-%H:%M'), currentBodyDosage))
+
+        RecursiveDoseInterval(dose=dose, doseHourInterval=doseHourInterval, dosesN=dosesN, halfLifeHours=halfLifeHours, bodyDosage=(currentBodyDosage), dosesTaken=(dosesTaken + 1))
+
+    else:
+        ax = plt.axes()
+        # plotting requires work as the x-axis is easily filled and unreadable when dosesN gets large.
+        # when this is added strftime can include month-day-hour:minute
+        plt.plot([time[0] for time in doseOverTime], [dose[1] for dose in doseOverTime])
+        plt.show() 
+        return doseOverTime
+
+
+# function calls:
+
+RecursiveDoseInterval(200, 24, 7, 18)
+#print(HowManyHalfCycles(200, 4, 0.05))
