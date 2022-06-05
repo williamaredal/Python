8d29b7e0baa0c007891543ee8fa9d507905f0439
William Peter Åredal <williamaredal@hotmail.com>
2022-06-05 10:30:58 +0200

    Fixed bugs and finished game

--- Games/2Player_Hangman.py
+++ Games/2Player_Hangman.py
@@ -1,13 +1,20 @@
+from getpass import getpass
 import enchant
 import os
 
-def DisplayWord(p1Word, p1Guesses, p2Word, p2Guesses):
+def IntroScreen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Welcome to 2 player hangman.\n")
+    print("Your inputs will be hidden from your opponent, but fear not! You are indeed typing.\n\n")
+
+
+def GameScreen(p1Word, p1Guesses, p2Word, p2Guesses):
     p1HiddenWord = ""
     p2HiddenWord = ""
 
     for word in p1Word:
         for letter in word:
-            if letter in p1Guesses:
+            if letter in p2Guesses:
                 p1HiddenWord += letter
 
             else:
@@ -17,7 +24,7 @@ def DisplayWord(p1Word, p1Guesses, p2Word, p2Guesses):
 
     for word in p2Word:
         for letter in word:
-            if letter in p2Guesses:
+            if letter in p1Guesses:
                 p2HiddenWord += letter
 
             else:
@@ -27,12 +34,23 @@ def DisplayWord(p1Word, p1Guesses, p2Word, p2Guesses):
 
     # refreshes terminal to show new state of game
     os.system('cls' if os.name == 'nt' else 'clear')
-    print("Welcome to 2 player hangman.\n")
-    print("Word to guess for Player 1: ", p2HiddenWord)
-    print("Word to guess for Player 2: ", p1HiddenWord, "\n")
-    print("Player 1 guesses: ", p1Guesses)
-    print("Player 2 guesses: ", p2Guesses, "\n")
+    print("Player 1:")
+    print("Word to guess: ", p2HiddenWord)
+    print("Guesses: ", (p1Guesses if len(p1Guesses) > 0 else {}), "\n")
 
+    print("Player 2:")
+    print("Word to guess: ", p1HiddenWord)
+    print("Guesses: ", (p2Guesses if len(p2Guesses) > 0 else {}), "\n\n")
+
+
+def VictoryScreen(playerN, players, word):
+    opponents = [p for p in players]
+    opponents.remove(playerN)
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("\nPlayer ", playerN, " wins!")
+    print("Dun dun DUN! Player", opponents[0], "was sent to the gallows...")
+    print("The word given by player", opponents[0], "was: ", word)
 
 
 def GameLoop():
@@ -40,15 +58,18 @@ def GameLoop():
     hangmanWorld = {}
     gameOver = False
 
-    # players enter valid words for their opponents to guess, and initializes game variables
-    p1Word = input("Player 1, please enter a valid word for Player 2 to guess: ")
-    while englishDictionary.check(p1Word) == False:
-        p1Word = input("Player 1, please enter a valid word for Player 2 to guess: ")
+    IntroScreen()
 
-    p2Word = input("Player 2, please enter a valid word for Player 1 to guess: ")
+    # players enter valid words for their opponents to guess, and initializes game variables
+    p1Word = getpass(prompt="Player 1, please enter a valid word for Player 2 to guess: ")
+    while englishDictionary.check(p1Word) == False:
+        p1Word = getpass(prompt="Player 1, please enter a valid word for Player 2 to guess: ")
+
+    p2Word = getpass(prompt="Player 2, please enter a valid word for Player 1 to guess: ")
     while englishDictionary.check(p2Word) == False:
-        p2Word = input("Player 2, please enter a valid word for Player 1 to guess: ")
+        p2Word = getpass(prompt="Player 2, please enter a valid word for Player 1 to guess: ")
     
+    hangmanWorld["players"] = [1, 2]
     hangmanWorld["p1Word"] = p1Word
     hangmanWorld["p1Chars"] = set(p1Word)
     hangmanWorld["p1Guesses"] = set()
@@ -58,14 +79,16 @@ def GameLoop():
 
     # game loop begins
     while gameOver == False:
-        # players enter valid chars as their guesses
-        p1Letter = input("Player 1, please enter a single letter: ")
-        while len(p1Letter) != 1 or p1Letter.isalpha() == False or p1Letter in hangmanWorld["p1Guesses"]:
-            p1Letter = input("Player 1, please enter a single letter: ")
+        GameScreen(hangmanWorld["p1Word"], hangmanWorld["p1Guesses"], hangmanWorld["p2Word"], hangmanWorld["p2Guesses"])
 
-        p2Letter = input("Player 2, please enter a single letter: ")
+        # players enter valid chars as their guesses
+        p1Letter = getpass(prompt="Player 1, please enter a single letter: ")
+        while len(p1Letter) != 1 or p1Letter.isalpha() == False or p1Letter in hangmanWorld["p1Guesses"]:
+            p1Letter = getpass(prompt="Player 1, please enter a single letter: ")
+
+        p2Letter = getpass(prompt="Player 2, please enter a single letter: ")
         while len(p2Letter) != 1 or p2Letter.isalpha() == False or p2Letter in hangmanWorld["p2Guesses"]:
-            p2Letter = input("Player 2, please enter a single letter: ")
+            p2Letter = getpass(prompt="Player 2, please enter a single letter: ")
 
         # players enter valid words as their guesses
         hangmanWorld["p1Guesses"].add(p1Letter)
@@ -79,20 +102,17 @@ def GameLoop():
             gameOver = True
 
         elif p1Victory:
-            print("\n\nPlayer 1 wins!")
-            print("Dun dun DUN! Player 2 is sent to the gallows...")
-            print("The word player 2 chose was: ", hangmanWorld["p1Word"])
+            VictoryScreen(1, hangmanWorld["players"], hangmanWorld["p2Word"])
             gameOver = True
         
         elif p2Victory:
-            print("\n\nPlayer 2 wins!")
-            print("Dun dun DUN! Player 1 is sent to the gallows...")
-            print("The word player 1 chose was: ", hangmanWorld["p2Word"])
+            VictoryScreen(2, hangmanWorld["players"], hangmanWorld["p1Word"])
             gameOver = True
         
         else:
-            DisplayWord(hangmanWorld["p1Word"], hangmanWorld["p1Guesses"],  hangmanWorld["p2Word"], hangmanWorld["p2Guesses"])        
+            GameScreen(hangmanWorld["p1Word"], hangmanWorld["p1Guesses"],  hangmanWorld["p2Word"], hangmanWorld["p2Guesses"])        
 
-        
 
-GameLoop()
+
+if __name__ == "__main__":
+    GameLoop()
